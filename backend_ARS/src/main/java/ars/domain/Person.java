71fa1e627a39b9141c11835b00687133ae4a9c00
batkhongor <batkhongop@gmail.com>
2021-05-09 20:58:26 -0500

    Created empty project for
    Created empty project for Appointment Reservation Software (ARS)

---
+++ backend_ARS/src/main/java/ars/domain/Person.java
@@ -0,0 +1,19 @@
+package ars.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String firstname;
+	private String lastname;
+	
+	
+}
