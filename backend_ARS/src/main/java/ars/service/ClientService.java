9d4b7507ed490f1e577209e6a6b6af4ba9d03132
BoazTurya <63027279+BoazTurya@users.noreply.github.com>
2021-05-11 00:11:30 -0500

    edited clientSerice

--- backend_ARS/src/main/java/ars/service/ClientService.java
+++ backend_ARS/src/main/java/ars/service/ClientService.java
@@ -1,5 +1,20 @@
 package ars.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import ars.domain.Appointment;
+import ars.domain.Person;
+
 public interface ClientService {
+
+	List<Appointment> findAllClientAppointments(Integer ClientId);
+	
+	void addNewAppointment(Person client,LocalDate date, Integer timeInHours);
+	
+	void deleteAppointment(Integer appointmentId);
+	
+	void editAppointment(Integer appointmentId);
+	
 
 }
--- backend_ARS/src/main/java/ars/service/impl/ClientServiceImpl.java
+++ backend_ARS/src/main/java/ars/service/impl/ClientServiceImpl.java
@@ -1,22 +1,55 @@
 package ars.service.impl;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ars.domain.Appointment;
+import ars.domain.Person;
+import ars.domain.Session;
 import ars.repository.AppointmentRepository;
+import ars.repository.SessionRepository;
 import ars.service.ClientService;
 
 
 @Service
 public class ClientServiceImpl implements ClientService	 {
+	@Autowired
 	AppointmentRepository appointmentRepository;
+	@Autowired
+	SessionRepository sessionRepository;
 	
+	@Override
 	public List<Appointment> findAllClientAppointments(Integer ClientId){
 		return appointmentRepository.findAll().stream().filter(a->a.getClient().getId()==ClientId)
 					.collect(Collectors.toList());
+	}
+	@Override
+	public void addNewAppointment(Person client,LocalDate date, Integer timeInHours) {
+	
+		Session requestedSession = sessionRepository.findAll().stream()
+										.filter(s->s.getDate().equals(date))
+										.filter(s->s.getStartTime()==timeInHours).findAny()
+										.orElseThrow(()->new NoSuchElementException("No session found at this date/time"));
+		LocalDate currentDate = LocalDate.now();
+		Appointment newAppointment = new Appointment(currentDate, client, requestedSession);
+		
+		appointmentRepository.save(newAppointment);
+		
+	}
+	@Override
+	public void deleteAppointment(Integer appointmentId) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void editAppointment(Integer appointmentId) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
