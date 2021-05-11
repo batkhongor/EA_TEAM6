988e6fbe4ef8bc18ad78451f675660ee235e72ae
Batdelger <obatdelger@gmail.com>
2021-05-11 17:36:50 -0500

    1 more person in startuprunner

--- backend_ARS/src/main/java/ars/StartupRunner.java
+++ backend_ARS/src/main/java/ars/StartupRunner.java
@@ -1,10 +1,6 @@
 package ars;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,26 +10,28 @@ import org.springframework.transaction.annotation.Transactional;
 
 import ars.domain.Person;
 import ars.domain.RoleType;
-import ars.service.impl.PersonServiceImpl;
+import ars.service.PersonService;
 
 @Component
 public class StartupRunner implements CommandLineRunner {
-	
+
 	@Autowired
-	private PersonServiceImpl personServiceImpl;
-	
-    @Override
-    @Transactional
-    public void run(String...args) throws Exception {
-    	
-    	//List<List<Integer>> result=new ArrayList<>();
-    	Set<RoleType> roleTypes=new HashSet<>();
-    	roleTypes.add( RoleType.ADMIN);
-    	
-    	Person person1=new Person("John", "Carter", "john", "john", roleTypes);
-    	personServiceImpl.createPerson(person1);
-    	System.out.println(person1.getRoles().toString());
-    	//result.size()
-    	//personRepository.save(admin);
-    }
+	private PersonService personService;
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+
+		// List<List<Integer>> result=new ArrayList<>();
+		Set<RoleType> roleTypes = new HashSet<>();
+		roleTypes.add(RoleType.ADMIN);
+
+		Person person1 = new Person("John", "Carter", "john", "john", roleTypes);
+		Person person2 = new Person("Batdelger", "O", "batdelger@mail.com", "mypass", roleTypes);
+		personService.createPerson(person1);
+		personService.createPerson(person2);
+		System.out.println(person1.getRoles().toString());
+		// result.size()
+		// personRepository.save(admin);
+	}
 }
