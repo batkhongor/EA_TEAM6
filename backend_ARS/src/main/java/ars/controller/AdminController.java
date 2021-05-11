6186d915a01f86a17582170209504ebe88589f3b
Batdelger <obatdelger@gmail.com>
2021-05-11 18:42:47 -0500

    tmp

--- backend_ARS/src/main/java/ars/controller/AdminController.java
+++ backend_ARS/src/main/java/ars/controller/AdminController.java
@@ -9,7 +9,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ars.domain.Person;
 import ars.domain.Session;
 import ars.dto.PersonDTO;
+import ars.service.AdminService;
 import ars.service.PersonService;
 
 @RestController
@@ -34,7 +34,9 @@ public class AdminController {
 	@Autowired
 	private PersonService personService;
 
-	
+	@Autowired
+	private AdminService adminService;
+
 	/* <PERSON> */
 	@GetMapping("/persons")
 	public List<PersonDTO> getPersonList() {
@@ -43,9 +45,9 @@ public class AdminController {
 	}
 
 	@GetMapping(value = "/persons", params = "paged=true")
-	public ResponseEntity<Page<PersonDTO>> getPersonList(Pageable pageable) {
+	public Page<PersonDTO> getPersonList(Pageable pageable) {
 		Page<Person> page = personService.findAllPaged(pageable);
-		return ResponseEntity.ok(page.map(this::convertToPersonDto));
+		return page.map(this::convertToPersonDto);
 	}
 
 	@GetMapping("/persons/{id}")
@@ -74,16 +76,48 @@ public class AdminController {
 		personService.deletePerson(personId);
 	}
 	/* </PERSON> */
-	
+
 	/* <SESSION> */
-	@GetMapping("/persons")
+	@GetMapping("/sessions")
 	public List<Session> getSessionList() {
-		List<Person> list = personService.findAll();
-		return list.stream().map(this::convertToPersonDto).collect(Collectors.toList());
+		List<Session> list = adminService.findAllSessions();
+		return list;// .stream().map(this::convertToPersonDto).collect(Collectors.toList());
+	}
+
+	@GetMapping(value = "/sessions", params = "paged=true")
+	public Page<Session> getSessionList(Pageable pageable) {
+		Page<Session> page = adminService.findAllSessions(pageable);
+		return page; // page.map(this::convertToPersonDto);
+	}
+
+	@GetMapping("/sessions/{id}")
+	public Session getSession(@PathVariable("id") Integer personId) {
+		Session entity = adminService.findSessionById(personId).get();
+		return entity;// convertToPersonDto(entity);
+	}
+
+	@PostMapping("/sessions")
+	public Session createSession(@Valid @RequestBody Session personDto) {
+		Session entity = personDto; // convertToEntity(personDto);
+		entity = adminService.createSession(entity);
+		return entity;// convertToPersonDto(entity);
+	}
+
+	@PutMapping("/sessions/{id}")
+	public Session updateSession(@PathVariable("id") Integer sessionId, @Valid @RequestBody Session sessionDto) {
+		adminService.findSessionById(sessionId).orElseThrow(RuntimeException::new);
+		Session entity = sessionDto;// convertToEntity(sessionDto);
+		entity = adminService.updateSession(entity);
+		return entity;// convertToPersonDto(entity);
+	}
+
+	@DeleteMapping("/sessions/{id}")
+	public void deleteSession(@PathVariable("id") Integer personId) {
+		personService.deletePerson(personId);
 	}
 
 	/* </SESSION> */
-	
+
 	/* <private methods> */
 	private PersonDTO convertToPersonDto(Person person) {
 		PersonDTO pDto = modelMapper.map(person, PersonDTO.class);
