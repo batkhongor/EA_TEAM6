eff2316c3369a7a1bdebd89ad1b4f3fbf181630d
Batdelger <obatdelger@gmail.com>
2021-05-12 12:06:01 -0500

    Session.startTime changed to LocalTime

--- backend_ARS/src/main/java/ars/StartupRunner.java
+++ backend_ARS/src/main/java/ars/StartupRunner.java
@@ -1,6 +1,7 @@
 package ars;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,42 +22,43 @@ public class StartupRunner implements CommandLineRunner {
 
 	@Autowired
 	private PersonService personServiceImpl;
-	
+
 	@Autowired
 	private SessionRepository sessionRepo;
-	
-    @Override
-    @Transactional
-    public void run(String...args) throws Exception {
-    	
-    	//List<List<Integer>> result=new ArrayList<>();
-    	
-    	// ------------ ADMIN ------------
-    	Set<RoleType> roleTypes=new HashSet<>();
-    	roleTypes.add(RoleType.ADMIN);
-    	roleTypes.add(RoleType.CUSTOMER);
-    	
-    	Person person1=new Person("John", "Carter", "john", "john", roleTypes);
-    	personServiceImpl.createPerson(person1);
-    	System.out.println(person1.getRoles().toString());
-    	
-    	// ------------ PROVIDER ------------
-    	Person provider1 = new Person("Provider", "Mr", "provider", "john", new HashSet<>());
-    	provider1.addRole(RoleType.PROVIDER);
-    	personServiceImpl.createPerson(provider1);
-    	
-    	LocalDate today = LocalDate.now();
-    	
-    	// ------------ RANDOM SESSIONS ------------
-    	for (int time=16; time < 24; time++) {
-    		Session session1 = new Session(today.plus(time, ChronoUnit.DAYS), time, 30, "Iowa", provider1);
-    		sessionRepo.save(session1);
-    	}
-    	
-    	// ------------ CUSTOMER ------------
-    	Person customer1 = new Person("Customer", "Mr", "customer", "john", new HashSet<>());
-    	customer1.addRole(RoleType.CUSTOMER);
-    	
-    	personServiceImpl.createPerson(customer1);
-    }
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+
+		// List<List<Integer>> result=new ArrayList<>();
+
+		// ------------ ADMIN ------------
+		Set<RoleType> roleTypes = new HashSet<>();
+		roleTypes.add(RoleType.ADMIN);
+		roleTypes.add(RoleType.CUSTOMER);
+
+		Person person1 = new Person("John", "Carter", "john", "john", roleTypes);
+		personServiceImpl.createPerson(person1);
+		System.out.println(person1.getRoles().toString());
+
+		// ------------ PROVIDER ------------
+		Person provider1 = new Person("Provider", "Mr", "provider", "john", new HashSet<>());
+		provider1.addRole(RoleType.PROVIDER);
+		personServiceImpl.createPerson(provider1);
+
+		LocalDate today = LocalDate.now();
+
+		// ------------ RANDOM SESSIONS ------------
+		for (int time = 16; time < 24; time++) {
+			Session session1 = new Session(today.plus(time, ChronoUnit.DAYS), LocalTime.of(time, 0), 30, "Iowa",
+					provider1);
+			sessionRepo.save(session1);
+		}
+
+		// ------------ CUSTOMER ------------
+		Person customer1 = new Person("Customer", "Mr", "customer", "john", new HashSet<>());
+		customer1.addRole(RoleType.CUSTOMER);
+
+		personServiceImpl.createPerson(customer1);
+	}
 }
--- backend_ARS/src/main/java/ars/domain/Session.java
+++ backend_ARS/src/main/java/ars/domain/Session.java
@@ -1,19 +1,32 @@
 package ars.domain;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
-import java.util.Set;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
-import ars.domain.Appointment;
-import ars.domain.Person;
-import lombok.*;
-import org.hibernate.validator.constraints.Length;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@NoArgsConstructor @Getter @Setter
+@NoArgsConstructor
+@Getter
+@Setter
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Session {
@@ -21,41 +34,33 @@ public class Session {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-
 	@Column(nullable = false)
 	@Future
 	private LocalDate date;
 
+	@Column(nullable = false)
+	@NotNull
+	private LocalTime startTime; // 0 - 24
 
 	@Column(nullable = false)
 	@NotNull
-	@DecimalMin(value="0",inclusive=true)
-	@DecimalMax(value="24",inclusive=false)
-	private Integer startTime; // 0 - 24
-
-
-
-	@Column(nullable = false)
-	@NotNull
-	@Max(value=60)
-	@Min(value=1)
+	@Max(value = 60)
+	@Min(value = 1)
 	private int duration; // in minutes
-
 
 	@Column(length = 50, nullable = false)
 	@NotNull
 	private String location;
 
 	@ManyToOne
-	@JoinColumn(name="provider_id", nullable = false)
+	@JoinColumn(name = "provider_id", nullable = false)
 	private Person provider;
 
-	@OneToMany(mappedBy="session")
+	@OneToMany(mappedBy = "session")
 	private List<Appointment> appointmentRequests;
 
-	public Session( @Future LocalDate date,
-			@NotNull @DecimalMin(value = "0", inclusive = true) @DecimalMax(value = "24", inclusive = false) Integer startTime,
-			@NotNull @Max(60) @Min(1) int duration, @NotNull String location, Person provider) {
+	public Session(@Future LocalDate date, @NotNull LocalTime startTime, @NotNull @Max(60) @Min(1) int duration,
+			@NotNull String location, Person provider) {
 		super();
 		this.date = date;
 		this.startTime = startTime;
@@ -63,6 +68,5 @@ public class Session {
 		this.location = location;
 		this.provider = provider;
 	}
-	
 
 }
